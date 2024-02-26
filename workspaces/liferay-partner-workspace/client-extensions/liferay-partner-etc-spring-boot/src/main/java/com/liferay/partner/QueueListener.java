f9accfff23f0defbc95cb849279968fe0e891f97
amosfong <amos.fong@liferay.com>
2024-02-26 09:50:18 -0800

    ISSD-2694 SF

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-etc-spring-boot/src/main/java/com/liferay/partner/QueueListener.java
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-etc-spring-boot/src/main/java/com/liferay/partner/QueueListener.java
@@ -58,328 +58,65 @@ public class QueueListener {
 		Message message, Channel channel,
 		@Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
 
-		String receivedRoutingKey = message.getMessageProperties(
+		String routingKey = message.getMessageProperties(
 		).getReceivedRoutingKey();
 
 		if (_log.isInfoEnabled()) {
 			_log.info(
 				StringBundler.concat(
-					"Receiving message ", deliveryTag, " with routing key ",
-					receivedRoutingKey));
+					"Received message ", deliveryTag, " with routing key ",
+					routingKey));
+		}
+
+		if (!routingKey.equals("koroneiki.account.create") &&
+			!routingKey.equals("koroneiki.account.update")) {
+
+			return;
 		}
 
 		try {
 			String body = new String(message.getBody(), "UTF-8");
 
-			if ((body == null) || body.isEmpty()) {
-				if (_log.isWarnEnabled()) {
-					_log.warn("Contained no data");
-				}
+			JSONObject jsonObject = new JSONObject(body);
 
+			JSONObject koroneikiAccountJSONObject = jsonObject.getJSONObject(
+				"account");
+
+			String salesforceAccountKey = _getSalesforceKey(
+				koroneikiAccountJSONObject);
+
+			if (salesforceAccountKey == null) {
 				channel.basicReject(deliveryTag, false);
 
 				return;
 			}
 
-			if (receivedRoutingKey.equals("koroneiki.account.update") ||
-				receivedRoutingKey.equals("koroneiki.account.create")) {
-
-				JSONObject jsonObject = new JSONObject(body);
-
-				JSONObject accountJSONObject = jsonObject.getJSONObject(
-					"account");
-
-				// JSONArray entitlementsJSONArray =
-				// 	accountJSONObject.getJSONArray("entitlements");
-
-				// if (entitlementsJSONArray == null) {
-				// 	channel.basicReject(deliveryTag, false);
-
-				// 	return;
-				// }
-
-				// boolean partner = false;
-
-				// for (int i = 0; i < entitlementsJSONArray.length(); i++) {
-				// 	JSONObject entitlementJSONObject =
-				// 		entitlementsJSONArray.getJSONObject(i);
-
-				// 	if (StringUtil.equalsIgnoreCase(
-				// 			entitlementJSONObject.getString("name"),
-				// 			"Partner")) {
-
-				// 		partner = true;
-
-				// 		break;
-				// 	}
-				// }
-
-				// if (!partner) {
-				// 	channel.basicReject(deliveryTag, false);
-
-				// 	return;
-				// }
-
-				JSONArray externalLinksJSONArray =
-					accountJSONObject.getJSONArray("externalLinks");
-
-				String accountName = accountJSONObject.getString("name");
-
-				JSONObject partnerAccountJSONObject = new JSONObject() {
-					{
-						put("name", accountName);
-					}
-				};
-
-				for (int i = 0; i < externalLinksJSONArray.length(); i++) {
-					JSONObject externalLinkJSONObject =
-						externalLinksJSONArray.getJSONObject(i);
-
-					String salesforceAccountKey = null;
-
-					if ((StringUtil.equalsIgnoreCase(
-							externalLinkJSONObject.getString("domain"),
-							"salesforce") ||
-						 StringUtil.equalsIgnoreCase(
-							 externalLinkJSONObject.getString("domain"),
-							 "dossiera")) &&
-						StringUtil.equalsIgnoreCase(
-							externalLinkJSONObject.getString("entityName"),
-							"account")) {
-
-						salesforceAccountKey = externalLinkJSONObject.getString(
-							"entityId");
-
-						partnerAccountJSONObject.put(
-							"externalReferenceCode", salesforceAccountKey);
-
-						break;
-					}
-
-					if (salesforceAccountKey == null) {
-						channel.basicReject(deliveryTag, false);
-
-						return;
-					}
-				}
-
-				JSONArray postalAddressesJSONArray =
-					accountJSONObject.getJSONArray("postalAddresses");
-
-				for (int i = 0; i < postalAddressesJSONArray.length(); i++) {
-					JSONObject postalAddressesJSONObject =
-						postalAddressesJSONArray.getJSONObject(i);
-
-					if (postalAddressesJSONObject.getBoolean("primary") &&
-						postalAddressesJSONObject.has("addressCountry")) {
-
-						for (String countryISOCode : Locale.getISOCountries()) {
-							Locale countryNameLocale = new Locale(
-								"", countryISOCode);
-
-							if (StringUtil.equalsIgnoreCase(
-									postalAddressesJSONObject.getString(
-										"addressCountry"),
-									countryNameLocale.toString())) {
-
-								partnerAccountJSONObject.put(
-									"partnerCountry", countryISOCode);
-							}
-						}
-					}
-				}
-
-				String accountExternalReferenceCode =
-					partnerAccountJSONObject.getString("externalReferenceCode");
-
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						StringBundler.concat(
-							"Updating account ", accountExternalReferenceCode,
-							" with name ", accountName));
-				}
-
-				try {
-					JSONObject proxyAccountJSONObject = _get(
-						uriBuilder -> uriBuilder.path(
-							"/o/c/proxyaccounts/by-external-reference-code/" +
-								accountExternalReferenceCode
-						).build());
-
-					if (proxyAccountJSONObject.has("partnerLevelType")) {
-						JSONObject partnerLevelsResponseJSONObject = _get(
-							uriBuilder -> uriBuilder.path(
-								"/o/c/partnerlevels/"
-							).queryParam(
-								"pageSize", "-1"
-							).build());
-
-						Long partnerLevelResponseTotalCoult =
-							partnerLevelsResponseJSONObject.getLong(
-								"totalCount");
-
-						if (partnerLevelResponseTotalCoult > 0) {
-							JSONObject partnerLevelTypeJSONObject =
-								proxyAccountJSONObject.getJSONObject(
-									"partnerLevelType");
-
-							JSONArray partnerLevelsResponseJSONArray =
-								partnerLevelsResponseJSONObject.getJSONArray(
-									"items");
-
-							for (int i = 0;
-								 i < partnerLevelsResponseJSONArray.length();
-								 i++) {
-
-								JSONObject partnerLevelResponseJSONObject =
-									partnerLevelsResponseJSONArray.
-										getJSONObject(i);
-
-								JSONObject partnerLevelTypeResponseJSONObject =
-									partnerLevelResponseJSONObject.
-										getJSONObject("partnerLevelType");
-
-								if (StringUtil.equalsIgnoreCase(
-										partnerLevelTypeResponseJSONObject.
-											getString("key"),
-										partnerLevelTypeJSONObject.getString(
-											"key"))) {
-
-									partnerAccountJSONObject.put(
-										"r_prtLvlToAcc_c_partnerLevelERC",
-										partnerLevelResponseJSONObject.
-											getString("externalReferenceCode"));
-								}
-							}
-						}
-					}
-
-					if (proxyAccountJSONObject.has("currency")) {
-						JSONObject accountCurrencyJSONObject =
-							proxyAccountJSONObject.getJSONObject("currency");
-
-						String accountCurrency =
-							accountCurrencyJSONObject.getString("key");
-
-						partnerAccountJSONObject.put(
-							"currency", accountCurrency);
-					}
-
-					String updatedAccount = _put(
-						partnerAccountJSONObject.toString(),
-						StringBundler.concat(
-							"/o/headless-admin-user/v1.0/accounts",
-							"/by-external-reference-code/",
-							accountExternalReferenceCode));
-
-					if (proxyAccountJSONObject.has("region")) {
-						JSONObject globalOrganizationResponseJSONObject = _get(
-							uriBuilder -> uriBuilder.path(
-								"/o/headless-admin-user/v1.0/organizations" +
-									"/by-external-reference-code/PRM-ORG-GLOBAL"
-							).build());
-
-						Long globalOrganizationId =
-							globalOrganizationResponseJSONObject.getLong("id");
-
-						JSONObject organizationsResponseJSONObject = _get(
-							uriBuilder -> uriBuilder.path(
-								"/o/headless-admin-user/v1.0/organizations/" +
-									globalOrganizationId +
-										"/child-organizations"
-							).queryParam(
-								"pageSize", "-1"
-							).build());
-
-						Long organizationsResponseTotalCount =
-							organizationsResponseJSONObject.getLong(
-								"totalCount");
-
-						if (organizationsResponseTotalCount > 0) {
-							String accountRegion =
-								proxyAccountJSONObject.getString("region");
-
-							JSONArray organizationsJSONArray =
-								organizationsResponseJSONObject.getJSONArray(
-									"items");
-
-							for (int i = 0; i < organizationsJSONArray.length();
-								 i++) {
-
-								JSONObject organizationJSONObject =
-									organizationsJSONArray.getJSONObject(i);
-
-								if (accountRegion.equals(
-										organizationJSONObject.getString(
-											"name"))) {
-
-									Long regionId =
-										organizationJSONObject.getLong("id");
-
-									if (_log.isInfoEnabled()) {
-										_log.info(
-											"Assigning Account to " +
-												accountRegion);
-									}
-
-									JSONObject updatedAccountJSONObject =
-										new JSONObject(updatedAccount);
-
-									JSONArray organizationIdsJSONArray =
-										updatedAccountJSONObject.getJSONArray(
-											"organizationIds");
-
-									if (organizationIdsJSONArray.isEmpty()) {
-										_post(
-											"",
-											StringBundler.concat(
-												"/o/headless-admin-user/v1.0",
-												"/accounts",
-												"/by-external-reference-code/",
-												accountExternalReferenceCode,
-												"/organizations/", regionId));
-
-										break;
-									}
-
-									Long organizationId =
-										organizationIdsJSONArray.getLong(0);
-
-									if (organizationId != regionId) {
-										JSONArray
-											accountExternalReferenceCodeJSONArray =
-												new JSONArray();
-
-										accountExternalReferenceCodeJSONArray.
-											put(accountExternalReferenceCode);
-
-										_patch(
-											accountExternalReferenceCodeJSONArray.
-												toString(),
-											StringBundler.concat(
-												"/o/headless-admin-user/v1.0",
-												"/organizations/move-accounts/",
-												organizationId, "/", regionId,
-												"/by-external-reference-code"));
-									}
-								}
-							}
-						}
-					}
-
-					channel.basicAck(deliveryTag, false);
-				}
-				catch (Exception exception) {
-					_log.error(exception);
-
-					channel.basicReject(deliveryTag, false);
-				}
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					StringBundler.concat(
+						"Updating account ", salesforceAccountKey,
+						" with name ",
+						koroneikiAccountJSONObject.getString("name")));
 			}
+
+			String countryISOCode = _getCountryISOCode(
+				koroneikiAccountJSONObject);
+
+			_updateAccount(
+				salesforceAccountKey,
+				koroneikiAccountJSONObject.getString("name"), countryISOCode);
 
 			channel.basicAck(deliveryTag, false);
 		}
-		catch (Exception exception) {
-			_log.error(exception);
+		catch (Exception exception1) {
+			_log.error(exception1);
+
+			try {
+				channel.basicReject(deliveryTag, false);
+			}
+			catch (Exception exception2) {
+				_log.error(exception2);
+			}
 		}
 	}
 
@@ -400,6 +137,136 @@ public class QueueListener {
 			).block());
 	}
 
+	private String _getCountryISOCode(JSONObject koroneikiAccountJSONObject) {
+		JSONArray postalAddressesJSONArray =
+			koroneikiAccountJSONObject.getJSONArray("postalAddresses");
+
+		for (int i = 0; i < postalAddressesJSONArray.length(); i++) {
+			JSONObject postalAddressesJSONObject =
+				postalAddressesJSONArray.getJSONObject(i);
+
+			if (postalAddressesJSONObject.getBoolean("primary") &&
+				postalAddressesJSONObject.has("addressCountry")) {
+
+				for (String countryISOCode : Locale.getISOCountries()) {
+					Locale countryNameLocale = new Locale("", countryISOCode);
+
+					if (StringUtil.equalsIgnoreCase(
+							postalAddressesJSONObject.getString(
+								"addressCountry"),
+							countryNameLocale.toString())) {
+
+						return countryISOCode;
+					}
+				}
+			}
+		}
+
+		return "";
+	}
+
+	private String _getPartnerLevelExternalReferenceCode(
+		JSONObject proxyAccountJSONObject) {
+
+		if (!proxyAccountJSONObject.has("partnerLevelType")) {
+			return "";
+		}
+
+		JSONObject partnerLevelsJSONObject = _get(
+			uriBuilder -> uriBuilder.path(
+				"/o/c/partnerlevels/"
+			).queryParam(
+				"pageSize", "-1"
+			).build());
+
+		Long partnerLevelTotalCount = partnerLevelsJSONObject.getLong(
+			"totalCount");
+
+		if (partnerLevelTotalCount > 0) {
+			JSONObject proxyPartnerLevelTypeJSONObject =
+				proxyAccountJSONObject.getJSONObject("partnerLevelType");
+
+			JSONArray partnerLevelsJSONArray =
+				partnerLevelsJSONObject.getJSONArray("items");
+
+			for (int i = 0; i < partnerLevelsJSONArray.length(); i++) {
+				JSONObject partnerLevelJSONObject =
+					partnerLevelsJSONArray.getJSONObject(i);
+
+				JSONObject partnerLevelTypeJSONObject =
+					partnerLevelJSONObject.getJSONObject("partnerLevelType");
+
+				if (StringUtil.equalsIgnoreCase(
+						partnerLevelTypeJSONObject.getString("key"),
+						proxyPartnerLevelTypeJSONObject.getString("key"))) {
+
+					return partnerLevelJSONObject.getString(
+						"externalReferenceCode");
+				}
+			}
+		}
+
+		return "";
+	}
+
+	private long _getRegionOrganizationId(String regionName) {
+		JSONObject globalOrganizationJSONObject = _get(
+			uriBuilder -> uriBuilder.path(
+				"/o/headless-admin-user/v1.0/organizations" +
+					"/by-external-reference-code/PRM-ORG-GLOBAL"
+			).build());
+
+		JSONObject organizationsJSONObject = _get(
+			uriBuilder -> uriBuilder.path(
+				"/o/headless-admin-user/v1.0/organizations/" +
+					globalOrganizationJSONObject.getLong("id") +
+						"/child-organizations"
+			).queryParam(
+				"pageSize", "-1"
+			).build());
+
+		if (organizationsJSONObject.getLong("totalCount") <= 0) {
+			return 0;
+		}
+
+		JSONArray organizationsJSONArray = organizationsJSONObject.getJSONArray(
+			"items");
+
+		for (int i = 0; i < organizationsJSONArray.length(); i++) {
+			JSONObject organizationJSONObject =
+				organizationsJSONArray.getJSONObject(i);
+
+			if (regionName.equals(organizationJSONObject.getString("name"))) {
+				return organizationJSONObject.getLong("id");
+			}
+		}
+
+		return 0;
+	}
+
+	private String _getSalesforceKey(JSONObject koroneikiAccountJSONObject) {
+		JSONArray externalLinksJSONArray =
+			koroneikiAccountJSONObject.getJSONArray("externalLinks");
+
+		for (int i = 0; i < externalLinksJSONArray.length(); i++) {
+			JSONObject externalLinkJSONObject =
+				externalLinksJSONArray.getJSONObject(i);
+
+			if ((StringUtil.equalsIgnoreCase(
+					externalLinkJSONObject.getString("domain"), "salesforce") ||
+				 StringUtil.equalsIgnoreCase(
+					 externalLinkJSONObject.getString("domain"), "dossiera")) &&
+				StringUtil.equalsIgnoreCase(
+					externalLinkJSONObject.getString("entityName"),
+					"account")) {
+
+				return externalLinkJSONObject.getString("entityId");
+			}
+		}
+
+		return "";
+	}
+
 	private WebClient _getWebClient() {
 		return WebClient.builder(
 		).baseUrl(
@@ -415,8 +282,8 @@ public class QueueListener {
 		).build();
 	}
 
-	private String _patch(String bodyValue, String path) {
-		return _getWebClient(
+	private void _patch(String bodyValue, String path) {
+		_getWebClient(
 		).patch(
 		).uri(
 			uriBuilder -> uriBuilder.path(
@@ -437,8 +304,8 @@ public class QueueListener {
 		).block();
 	}
 
-	private String _post(String bodyValue, String path) {
-		return _getWebClient(
+	private void _post(String bodyValue, String path) {
+		_getWebClient(
 		).post(
 		).uri(
 			uriBuilder -> uriBuilder.path(
@@ -459,26 +326,123 @@ public class QueueListener {
 		).block();
 	}
 
-	private String _put(String bodyValue, String path) {
-		return _getWebClient(
-		).put(
-		).uri(
+	private JSONObject _put(String bodyValue, String path) {
+		return new JSONObject(
+			_getWebClient(
+			).put(
+			).uri(
+				uriBuilder -> uriBuilder.path(
+					path
+				).build()
+			).accept(
+				MediaType.APPLICATION_JSON
+			).contentType(
+				MediaType.APPLICATION_JSON
+			).header(
+				HttpHeaders.AUTHORIZATION,
+				"Bearer " + _oAuth2AccessToken.getTokenValue()
+			).bodyValue(
+				bodyValue
+			).retrieve(
+			).bodyToMono(
+				String.class
+			).block());
+	}
+
+	private void _updateAccount(
+			String externalReferenceCode, String name, String countryISOCode)
+		throws Exception {
+
+		JSONObject accountJSONObject = new JSONObject() {
+			{
+				put("externalReferenceCode", externalReferenceCode);
+				put("name", name);
+			}
+		};
+
+		JSONObject proxyAccountJSONObject = _get(
 			uriBuilder -> uriBuilder.path(
-				path
-			).build()
-		).accept(
-			MediaType.APPLICATION_JSON
-		).contentType(
-			MediaType.APPLICATION_JSON
-		).header(
-			HttpHeaders.AUTHORIZATION,
-			"Bearer " + _oAuth2AccessToken.getTokenValue()
-		).bodyValue(
-			bodyValue
-		).retrieve(
-		).bodyToMono(
-			String.class
-		).block();
+				"/o/c/proxyaccounts/by-external-reference-code/" +
+					externalReferenceCode
+			).build());
+
+		if (proxyAccountJSONObject.has("currency")) {
+			JSONObject proxyCurrencyJSONObject =
+				proxyAccountJSONObject.getJSONObject("currency");
+
+			accountJSONObject.put(
+				"currency", proxyCurrencyJSONObject.getString("key"));
+		}
+
+		if (!countryISOCode.equals("")) {
+			accountJSONObject.put("partnerCountry", countryISOCode);
+		}
+
+		String partnerLevelExternalReferenceCode =
+			_getPartnerLevelExternalReferenceCode(proxyAccountJSONObject);
+
+		if (!partnerLevelExternalReferenceCode.equals("")) {
+			accountJSONObject.put(
+				"r_prtLvlToAcc_c_partnerLevelERC",
+				partnerLevelExternalReferenceCode);
+		}
+
+		JSONObject updatedAccountJSONObject = _put(
+			accountJSONObject.toString(),
+			"/o/headless-admin-user/v1.0/accounts/by-external-reference-code/" +
+				externalReferenceCode);
+
+		if (proxyAccountJSONObject.has("region")) {
+			_updateAccountRegion(
+				updatedAccountJSONObject,
+				proxyAccountJSONObject.getString("region"));
+		}
+	}
+
+	private void _updateAccountRegion(
+		JSONObject accountJSONObject, String proxyAccountRegion) {
+
+		long regionOrganizationId = _getRegionOrganizationId(
+			proxyAccountRegion);
+
+		if (regionOrganizationId <= 0) {
+			return;
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Assigning account to " + proxyAccountRegion);
+		}
+
+		JSONArray organizationIdsJSONArray = accountJSONObject.getJSONArray(
+			"organizationIds");
+
+		if (organizationIdsJSONArray.isEmpty()) {
+			_post(
+				"",
+				StringBundler.concat(
+					"/o/headless-admin-user/v1.0/accounts",
+					"/by-external-reference-code/",
+					accountJSONObject.getString("externalReferenceCode"),
+					"/organizations/", regionOrganizationId));
+		}
+		else {
+			Long organizationId = organizationIdsJSONArray.getLong(0);
+
+			if (organizationId != regionOrganizationId) {
+				JSONArray accountExternalReferenceCodeJSONArray =
+					new JSONArray();
+
+				accountExternalReferenceCodeJSONArray.put(
+					accountJSONObject.getString("externalReferenceCode"));
+
+				_patch(
+					accountExternalReferenceCodeJSONArray.toString(),
+					StringBundler.concat(
+						"/o/headless-admin-user/v1.0/organizations",
+						"/move-accounts/", organizationId, "/",
+						regionOrganizationId, "/by-external-reference-code"));
+			}
+		}
 	}
 
 	private static final Log _log = LogFactory.getLog(QueueListener.class);
