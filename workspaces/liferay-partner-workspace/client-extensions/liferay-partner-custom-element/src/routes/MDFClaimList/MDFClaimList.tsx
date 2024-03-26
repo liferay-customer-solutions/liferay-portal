d91e51d97b13035d85d1ce9ca0a3f65afee54a49
Felipe França <felipe.franca@liferay.com>
2024-03-26 14:34:34 -0300

    ISSD-3168 Refactoring filters for mdf claim list page

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/src/routes/MDFClaimList/MDFClaimList.tsx
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/src/routes/MDFClaimList/MDFClaimList.tsx
@@ -9,7 +9,7 @@ import ClayIcon from '@clayui/icon';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
 import {ClayPaginationBarWithBasicItems} from '@clayui/pagination-bar';
 import ClayTabs from '@clayui/tabs';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {CSVLink} from 'react-csv';
 
 import Table from '../../common/components/Table';
@@ -27,6 +27,7 @@ import usePagination from '../../common/hooks/usePagination';
 import usePermissionActions from '../../common/hooks/usePermissionActions';
 import {MDFClaimListItem} from '../../common/interfaces/mdfClaimListItem';
 import TableColumn from '../../common/interfaces/tableColumn';
+import {Liferay} from '../../common/services/liferay';
 import {Filters} from '../../common/utils/constants/filters';
 import getDropDownFilterMenus from '../../common/utils/getDropDownFilterMenus';
 import useDynamicFieldEntries from './hooks/useDynamicFieldEntries';
@@ -53,12 +54,25 @@ const MDFClaimList = () => {
 
 	const {companiesEntries} = useDynamicFieldEntries();
 
+	const [urlParams] = useState(
+		new URLSearchParams(window.location.href.split('?')[1])
+	);
+	const urlParamsEntries = urlParams.entries();
+	useEffect(() => {
+		window.history.replaceState(
+			null,
+			'',
+			`${Liferay.ThemeDisplay.getLayoutRelativeURL()}?${urlParams.toString()}`
+		);
+	}, [urlParams, urlParamsEntries]);
+
 	const {filters, filtersTerm, onFilter, setFilters} = useFilters(
 		openClaimsFilter,
+		urlParams,
 		isChannel
 	);
 
-	const pagination = usePagination();
+	const pagination = usePagination(urlParams);
 	const {data, isValidating, mutate} = useGetListItemsFromMDFClaims(
 		pagination.activePage,
 		pagination.activeDelta,
@@ -75,7 +89,7 @@ const MDFClaimList = () => {
 
 	const actions = usePermissionActions(ObjectActionName.MDF_CLAIM);
 
-	const columns = getMDFClaimListColumns(siteURL, actions, mutate);
+	const columns = getMDFClaimListColumns(urlParams, siteURL, actions, mutate);
 
 	const getTable = (
 		totalCount: number,
@@ -142,6 +156,7 @@ const MDFClaimList = () => {
 									searchTerm,
 								})
 							}
+							urlParams={urlParams}
 						/>
 
 						<div className="bd-highlight flex-shrink-2 mt-1">
