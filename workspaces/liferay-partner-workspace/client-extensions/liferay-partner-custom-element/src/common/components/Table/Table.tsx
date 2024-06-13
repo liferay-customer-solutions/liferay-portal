cdbb596b73efadc915a6a7c194edc41cdda2a0e5
Elias Santos <elias.santos@liferay.com>
2024-06-13 17:17:48 -0300

    LRSD-4825 Fix the table component sort

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/src/common/components/Table/Table.tsx
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-custom-element/src/common/components/Table/Table.tsx
@@ -115,7 +115,11 @@ const Table = <T extends BasicRow>({
 }: TableProps<T>) => {
 	const [sort, setSort] = useState<Sorting | null>(null);
 
-	const getColumnkeyFromObjectString = (columnKey: string) => {
+	const getSortString = (
+		columnKey: string,
+		prevSort: string,
+		sortOrder: string
+	) => {
 		const columnMap: {[key: string]: string} = {
 			ACCOUNT_NAME: 'prospectAccountName',
 			CLAIM_STATUS: 'mdfClaimStatus',
@@ -131,7 +135,9 @@ const Table = <T extends BasicRow>({
 			TYPE: 'partial',
 		};
 
-		return columnMap[columnKey];
+		return columnMap[columnKey]
+			? `${columnMap[columnKey]}:${sortOrder}`
+			: prevSort;
 	};
 
 	const onSortChange = useCallback((sort: Sorting | null) => {
@@ -164,9 +170,11 @@ const Table = <T extends BasicRow>({
 											? 'desc'
 											: 'asc';
 
-										return `${getColumnkeyFromObjectString(
-											column.columnKey
-										)}:${sortOrder}`;
+										return getSortString(
+											column.columnKey,
+											prevSort,
+											sortOrder
+										);
 									});
 								}}
 								sortable={
