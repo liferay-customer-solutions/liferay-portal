97e2f6d6e9d2c37d64df7e7027fe7cc971ae7c5a
Joao Marcelo Alves <joao.alves@liferay.com>
2024-06-20 14:29:34 -0300

    LRSD-4911 Create base test setup for master

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -111,6 +111,7 @@ export default defineConfig({
 		objectWebConfig,
 		openIdLinkConfig,
 		osbFaroWebConfig,
+		partnerConfig,
 		portalDefaultPermissionsWebConfig,
 		portalSearchAdminWebConfig,
 		portalSearchWebConfig,
