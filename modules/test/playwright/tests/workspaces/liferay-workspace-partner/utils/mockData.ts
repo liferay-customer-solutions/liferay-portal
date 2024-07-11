a1c2d449163558d0f563c6b1b266a28a77319b69
Clarice Brasil <clarice.brasil@liferay.com>
2024-07-11 09:39:46 -0300

    LRSD-4428 Create tests for MDF Request List Page

---
+++ modules/test/playwright/tests/workspaces/liferay-workspace-partner/utils/mockData.ts
@@ -0,0 +1,22 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export const mockMDFRequestData = {
+	claimPercent:0.5,
+	companyName:"Deathray, Inc.*",
+	convertedTotalCostOfExpense:50000,
+	convertedTotalMDFRequestAmount:25000,
+	liferayBusinessSalesGoals:"Lead generation",
+	liferayBusinessSalesGoalsOther:"",
+	maxDateActivity:"2024-07-12",
+	minDateActivity:"2024-07-11",
+	overallCampaignDescription:"Campaign Description",
+	overallCampaignName:"Test Campaign",
+	submitDate:"2024-07-10T18:11:39.346Z",
+	targetAudienceRoles:"C-Level/Executive/VP; Independent Contractor",
+	targetMarkets:"Aerospace & Defense; Agriculture",
+	totalCostOfExpense:0,
+	totalMDFRequestAmount:25000					
+}
