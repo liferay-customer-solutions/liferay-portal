c71acba96cda066c18bb5aa6b54f38c5f8bb365b
Clarice Brasil <clarice.brasil@liferay.com>
2024-07-18 15:28:05 -0300

    LRSD-5100 Refactor MDF Request Unit Tests to new approach

--- modules/test/playwright/tests/workspaces/liferay-partner-workspace/tests/mdf/unit/mdfRequestForm.spec.ts
+++ modules/test/playwright/tests/workspaces/liferay-partner-workspace/tests/mdf/unit/mdfRequestForm.spec.ts
@@ -5,22 +5,48 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
+import { apiHelpersTest } from '../../../../../../fixtures/apiHelpersTest';
 import {partnerPagesTest} from '../../../fixtures/partnerPagesTest';
+import { generateMDFRequestData } from '../../../pages/mdf/utils/mdfRequestData';
 
-export const test = mergeTests(partnerPagesTest);
+export const test = mergeTests(apiHelpersTest, partnerPagesTest);
 
 test.describe('MDF Request Form', () => {
-	test('Open MDF Request Form', async ({
-		mdfRequestFormPage,
-		page,
-		partnerSite,
-	}) => {
-		await mdfRequestFormPage.goto(partnerSite.friendlyUrlPath);
+	const accountName = 'Deathray, Inc.*';
+	const accountRole = '[Account] Partner Manager (PM)';
+	let accountUser;
 
+	test.beforeEach(async ({mdfRequestFormPage, partnerHelper, partnerSite}) => {
+		const { account } = await partnerHelper.createAccountUser({accountName, accountType: 'business'});
+		await partnerHelper.assignUserToAccountRole({accountId: account.id, accountRole});
+
+		accountUser = account;
+
+		await mdfRequestFormPage.goto(partnerSite.friendlyUrlPath); 
+	});
+
+	test.afterEach(async ({apiHelpers}) => {
+		await apiHelpers.headlessAdminUser.deleteAccount(accountUser.id);
+	})
+
+	test('Open MDF Request Form', async ({page}) => {
 		const heading = await page.getByRole('heading', {
 			name: 'MDF Request',
 		});
 
 		expect(heading).toBeTruthy();
+	});
+
+	test('Create a New MDF Request', async ({
+		mdfRequestFormPage
+	}) => {
+		const mdfRequestData = generateMDFRequestData();
+
+		await mdfRequestFormPage.createNewRequest(mdfRequestData);
+		await mdfRequestFormPage.reviewMDFRequest(mdfRequestData);
+
+		await mdfRequestFormPage.submitButton.click();
+
+		await expect(mdfRequestFormPage.successMessage).toBeVisible();
 	});
 });
