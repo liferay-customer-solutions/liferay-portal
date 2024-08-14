5ead3037e3f9c0a534d35ed55ba0736b039d2e37
Clarice Brasil <clarice.brasil@liferay.com>
2024-08-14 09:19:37 -0300

    LRSD-5519 Create custom fragment to hide dropdown menu option

---
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-site-initializer-code/site-initializer/fragments/group/src/mdf-claim-detail-collection/fragments/form-dropdown-button/index.js
@@ -0,0 +1,59 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+const DROPDOWN_SELECT = 'input[name="r_campaign_c_campaignId"]';
+const FORM_CONTAINER =
+	'#_com_liferay_object_web_internal_object_definitions_portlet_ObjectDefinitionsPortlet_41445_fm';
+const FORM_GROUP = '.form-group';
+const HIDDEN_CLASS = 'hidden';
+const ITEM_OPTION = '.dropdown-item';
+const SELECT_OPTION = '.dropdown-menu.autocomplete-dropdown-menu.show';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const handleMutations = (mutations) => {
+		mutations.forEach(({addedNodes}) => {
+			if (addedNodes.length) {
+				const selectDropdown = document.querySelector(DROPDOWN_SELECT);
+
+				if (selectDropdown) {
+					selectDropdown
+						.closest(FORM_GROUP)
+						.classList.add(HIDDEN_CLASS);
+
+					setTimeout(() => {
+						selectDropdown.dispatchEvent(new Event('focus'));
+
+						const selectOption =
+							document.querySelector(SELECT_OPTION);
+
+						if (selectOption) {
+							const itemOption =
+								selectOption.querySelector(ITEM_OPTION);
+
+							if (itemOption) {
+								itemOption.click();
+							}
+						}
+					}, 200);
+				}
+			}
+		});
+	};
+
+	const observer = () => {
+		const formContainer = document
+			.querySelector(FORM_CONTAINER)
+			?.closest('form');
+
+		if (formContainer) {
+			const mutationObserver = new MutationObserver(handleMutations);
+			mutationObserver.observe(formContainer, {
+				childList: true,
+				subtree: true,
+			});
+		}
+	};
+
+	observer();
+});
