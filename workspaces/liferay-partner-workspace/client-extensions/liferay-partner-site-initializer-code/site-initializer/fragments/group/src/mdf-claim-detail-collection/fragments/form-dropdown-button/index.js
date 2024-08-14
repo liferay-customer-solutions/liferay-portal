9efff5f6cb400afd8e6793484ade4a6a3262bec5
Clarice Brasil <clarice.brasil@liferay.com>
2024-08-14 09:44:24 -0300

    LRSD-5519 SF

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-site-initializer-code/site-initializer/fragments/group/src/mdf-claim-detail-collection/fragments/form-dropdown-button/index.js
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-site-initializer-code/site-initializer/fragments/group/src/mdf-claim-detail-collection/fragments/form-dropdown-button/index.js
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 const DROPDOWN_SELECT = 'input[name="r_campaign_c_campaignId"]';
 const FORM_CONTAINER =
 	'#_com_liferay_object_web_internal_object_definitions_portlet_ObjectDefinitionsPortlet_41445_fm';
