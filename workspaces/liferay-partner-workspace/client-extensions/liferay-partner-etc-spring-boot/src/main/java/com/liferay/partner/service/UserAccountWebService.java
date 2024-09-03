6bd0c8ba6e828dc8595ed6e7f0a2af4ac7bab86f
Felipe Franca <felipe.franca@liferay.com>
2024-09-03 11:25:12 -0300

    LRSD-4211 SF

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-etc-spring-boot/src/main/java/com/liferay/partner/service/UserAccountWebService.java
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-etc-spring-boot/src/main/java/com/liferay/partner/service/UserAccountWebService.java
@@ -1,11 +1,14 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.partner.service;
 
+import com.liferay.client.extension.util.spring.boot.LiferayOAuth2AccessTokenManager;
+
 import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -14,25 +17,20 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.liferay.client.extension.util.spring.boot.LiferayOAuth2AccessTokenManager;
-
 /**
  * @author Felipe Franca
  */
 @Component
 public class UserAccountWebService {
 
-    public JSONObject getUserAccount(
-			Jwt jwt)
-		throws Exception {
-
-		JSONObject jsonObject = new JSONObject(
+	public JSONObject getUserAccount(Jwt jwt) throws Exception {
+		return new JSONObject(
 			WebClient.create(
 				_lxcDXPServerProtocol + "://" + _lxcDXPMainDomain
 			).get(
 			).uri(
-				"/o/headless-admin-user/v1.0/user-accounts/"
-				+ jwt.getClaimAsString("sub")
+				"/o/headless-admin-user/v1.0/user-accounts/" +
+					jwt.getClaimAsString("sub")
 			).accept(
 				MediaType.APPLICATION_JSON
 			).header(
@@ -41,19 +39,18 @@ public class UserAccountWebService {
 			).bodyToMono(
 				String.class
 			).block());
-
-		return jsonObject;
 	}
 
 	private String _getAuthorization() {
 		return _liferayOAuth2AccessTokenManager.getAuthorization(
-			"liferay-partner-etc-spring-boot-oauth-application-headless-server");
+			"liferay-partner-etc-spring-boot-oauth-application-headless-" +
+				"server");
 	}
 
 	@Autowired
 	private LiferayOAuth2AccessTokenManager _liferayOAuth2AccessTokenManager;
 
-    @Value("${com.liferay.lxc.dxp.mainDomain}")
+	@Value("${com.liferay.lxc.dxp.mainDomain}")
 	private String _lxcDXPMainDomain;
 
 	@Value("${com.liferay.lxc.dxp.server.protocol}")
