f61a3f21072f3b74d597511fd5ba47dfcfc18728
Felipe Franca <felipe.franca@liferay.com>
2024-09-18 09:16:03 -0300

    LRSD-5414 Make the test more generic

--- modules/test/playwright/tests/workspaces/liferay-customer-workspace/pages/HomePage.ts
+++ modules/test/playwright/tests/workspaces/liferay-customer-workspace/pages/HomePage.ts
@@ -10,16 +10,16 @@ import {CUSTOMER_SITE_FRIENLY_URL_PATH} from '../utils/constants';
 export class HomePage {
 	readonly heading: Locator;
 	readonly page: Page;
+	readonly projectCard: Locator;
 	readonly searchBar: Locator;
-	readonly testKoroneikiAccountCode: Locator;
 
 	constructor(page: Page) {
 		this.heading = page.getByRole('link', {
 			name: 'Customer Portal',
 		});
 		this.page = page;
+		this.projectCard = page.locator('.card-body').first();
 		this.searchBar = page.getByPlaceholder('Find a project');
-		this.testKoroneikiAccountCode = page.getByText('TESTACCOUNT01');
 	}
 
 	async goto() {
