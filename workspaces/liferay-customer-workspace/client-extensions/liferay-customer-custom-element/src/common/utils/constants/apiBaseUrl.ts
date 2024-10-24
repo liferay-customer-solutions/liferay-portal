9ca2ae2246cdf4bb11f580a8805617d01aeb9974
Ryan Schuhler <ryan.schuhler@liferay.com>
2024-10-24 15:03:54 -0700

    LRSD-6920 [LRSD-6921] Fix API_BASE_URL

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/common/utils/constants/apiBaseUrl.ts
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/common/utils/constants/apiBaseUrl.ts
@@ -3,8 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-const {REACT_APP_LIFERAY_API = window.location.origin} = process.env;
-
-const API_BASE_URL = REACT_APP_LIFERAY_API;
+const API_BASE_URL = window.location.origin;
 
 export {API_BASE_URL};
