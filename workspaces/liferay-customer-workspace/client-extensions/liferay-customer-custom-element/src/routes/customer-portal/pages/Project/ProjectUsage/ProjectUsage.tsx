1066e726008cdb8abefd513d4b9beab596fa0740
Ryan Schuhler <ryan.schuhler@liferay.com>
2024-11-22 16:05:55 -0800

    LRSD-7297 Refinements

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/routes/customer-portal/pages/Project/ProjectUsage/ProjectUsage.tsx
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/routes/customer-portal/pages/Project/ProjectUsage/ProjectUsage.tsx
@@ -7,6 +7,9 @@ import CardContainer from './components/CardContainer';
 import ProgressBarContent from './components/ProgressBarContent';
 
 import './ProjectUsage.css';
+
+import i18n from '~/common/I18n';
+
 import ChartContent from './components/ChartContent';
 import ProjectUsageSection from './components/ProjectUsageSection';
 import useProjectUsageData from './hooks/useProjectUsageData';
@@ -15,13 +18,13 @@ const ProjectUsage = () => {
 	const {isLoading, usageData} = useProjectUsageData();
 
 	return (
-		<div className="cp-project-usage-page m-0 p-0">
-			<h2 className="mb-5">Project Usage Metrics</h2>
+		<div className="container-xl cp-project-usage-page m-0 p-0">
+			<h2 className="mb-5">{i18n.translate('project-usage-metrics')}</h2>
 
 			<ProjectUsageSection
 				className="mb-5"
 				isLoading={isLoading}
-				title="Sites and Users"
+				title={i18n.translate('sites-and-users')}
 			>
 				{usageData?.siteAndUsers.map((chartData, index) => (
 					<CardContainer
@@ -40,11 +43,10 @@ const ProjectUsage = () => {
 			<ProjectUsageSection
 				className="mb-5"
 				isLoading={isLoading}
-				title="Resource Usage"
+				title={i18n.translate('resource-usage')}
 			>
 				{usageData?.resourceUsage.map((chartData, index) => (
 					<CardContainer
-						classNameCard="px-4"
 						infoButtonText={chartData.infoText}
 						key={`${chartData.title}-${index}`}
 					>
