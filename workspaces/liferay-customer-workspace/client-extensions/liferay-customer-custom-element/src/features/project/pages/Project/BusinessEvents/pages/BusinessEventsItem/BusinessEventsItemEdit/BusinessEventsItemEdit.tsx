f88d3b6082da78d35acbc354629d5da8ea04a3ec
Felipe Franca <felipe.franca@liferay.com>
2025-05-20 17:07:57 -0300

    LRSD-7714 Change business event status to open after user edits the event

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/project/pages/Project/BusinessEvents/pages/BusinessEventsItem/BusinessEventsItemEdit/BusinessEventsItemEdit.tsx
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/project/pages/Project/BusinessEvents/pages/BusinessEventsItem/BusinessEventsItemEdit/BusinessEventsItemEdit.tsx
@@ -192,6 +192,7 @@ const BusinessEventsItemEditPage: React.FC<IProps> = ({
 			associatedTickets: businessEvent.associatedTickets,
 			currentLiferayVersion: businessEvent.currentLiferayVersion?.key,
 			description: businessEvent.description,
+			eventStatus: 'open',
 			eventType: businessEvent.eventType?.key,
 			lastComment: reason,
 			name: businessEvent?.name,
