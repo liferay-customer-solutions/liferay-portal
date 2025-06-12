ead5cf2bafb349939b4bbed053faebf02432716f
Felipe Franca <felipe.franca@liferay.com>
2025-06-12 11:16:54 -0300

    LRSD-9810 Fix security vulnerability search

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/JiraRestController.java
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/JiraRestController.java
@@ -47,7 +47,8 @@ public class JiraRestController extends BaseRestController {
 				throw new PrincipalException();
 			}
 
-			_jiraService.scheduledCacheEviction();
+			_jiraService.scheduledAffectedVersionsCacheEviction();
+			_jiraService.scheduledIssuesCacheEviction();
 
 			return new ResponseEntity<>(HttpStatus.OK);
 		}
