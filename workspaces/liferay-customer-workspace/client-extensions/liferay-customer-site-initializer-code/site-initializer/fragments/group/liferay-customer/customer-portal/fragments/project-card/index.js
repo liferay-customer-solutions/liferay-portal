caa623fe7a6184b93695e480a401ef9548e056ef
Pedro Oliveira <pedro.oliveira@liferay.com>
2025-06-20 15:57:24 -0300

    LRSD-9417 Turn project card clickable

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-site-initializer-code/site-initializer/fragments/group/liferay-customer/customer-portal/fragments/project-card/index.js
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-site-initializer-code/site-initializer/fragments/group/liferay-customer/customer-portal/fragments/project-card/index.js
@@ -2,3 +2,23 @@
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
+document.addEventListener('DOMContentLoaded', () => {
+	const projectCardContent = document.querySelectorAll(
+		'.project-card-content'
+	);
+
+	projectCardContent.forEach((card) => {
+		card.addEventListener('click', function (event) {
+			const isInteractiveButton = event.target.closest('a, button');
+
+			if (!isInteractiveButton) {
+				const targetURL = this.dataset.href;
+
+				if (targetURL) {
+					window.location.href = targetURL;
+				}
+			}
+		});
+	});
+});
