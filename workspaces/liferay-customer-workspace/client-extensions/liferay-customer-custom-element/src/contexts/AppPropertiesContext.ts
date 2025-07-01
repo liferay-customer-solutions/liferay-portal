03676d0e66a3a1a3a1e2f9b80f4952e12b63d43f
Ryan Schuhler <ryan.schuhler@liferay.com>
2025-06-30 17:20:11 -0700

    LRSD-9634 Convert properties to envvars

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/contexts/AppPropertiesContext.ts
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/contexts/AppPropertiesContext.ts
@@ -6,23 +6,7 @@
 import {ApolloClient} from '@apollo/client';
 import {createContext, useContext} from 'react';
 
-export const AppPropertiesContext = createContext({
-	accountSettingsURL: '',
-	articleAccountSupportURL: '',
-	articleDeactivateKey: '',
-	articleDeployingActivationKeysURL: '',
-	articleGettingStartedWithLiferayEnterpriseSearchURL: '',
-	articleNotifiedWhenMyActivationKeyIsAboutToExpireURL: '',
-	articleWhatIsMyInstanceSizingValueURL: '',
-	client: null,
-	featureFlags: [] as string[],
-	gravatarAPI: '',
-	helpCenterURL: '',
-	importDate: null,
-	provisioningServerAPI: '',
-	submitSupportTicketURL: '',
-	theOverviewPageURL: '',
-});
+export const AppPropertiesContext = createContext({});
 
 export function useAppPropertiesContext() {
 	const context = useContext(AppPropertiesContext);
