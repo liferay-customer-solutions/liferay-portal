c4ef9d18ca9cbf75446aef4119a0b49b2f09c9e2
Carlos Correa <carlos.correa@liferay.com>
2026-05-05 12:57:16 +0200

    LPD-87965 Make the port dynamic in the JSON files and replace the port with the dynamic value

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/OpenAPIResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/OpenAPIResourceTest.java
@@ -30,6 +30,7 @@ import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -471,8 +472,11 @@ public class OpenAPIResourceTest {
 		throws Exception {
 
 		JSONAssert.assertEquals(
-			new String(
-				FileUtil.getBytes(getClass(), "dependencies/" + fileName)),
+			StringUtil.replace(
+				new String(
+					FileUtil.getBytes(getClass(), "dependencies/" + fileName)),
+				"localhost:<serverPort>",
+				"localhost:" + PortalUtil.getPortalServerPort(false)),
 			HTTPTestUtil.invokeToJSONObject(
 				null, objectDefinition.getRESTContextPath() + "/openapi.json",
 				Http.Method.GET
