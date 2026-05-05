efb705bfce6ecbe0067d8ca78d63ddcbaf1c7327
Rafael Praxedes <rafael.praxedes@liferay.com>
2026-05-05 17:50:39 -0300

    LPD-86315 move GlobalPrivacyControlProviderImpl out of provider subpackage

--- modules/apps/cookies/cookies-impl/src/main/java/com/liferay/cookies/internal/global/privacy/control/GlobalPrivacyControlProviderImpl.java
+++ modules/apps/cookies/cookies-impl/src/main/java/com/liferay/cookies/internal/global/privacy/control/GlobalPrivacyControlProviderImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.cookies.internal.global.privacy.control.provider;
+package com.liferay.cookies.internal.global.privacy.control;
 
 import com.liferay.cookies.configuration.CookiesConfigurationProvider;
 import com.liferay.cookies.global.privacy.control.GlobalPrivacyControlProvider;
