4f1d7d7412c9cc844bda5c95f65068b3f65dc186
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-05-05 10:51:51 +0200

    LPD-88398 Adapt tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/segment-editor/dynamic/__tests__/SegmentEditor.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/segment-editor/dynamic/__tests__/SegmentEditor.jsx
@@ -90,7 +90,7 @@ describe('SegmentEditor', () => {
 		expect(screen.getByTestId('toggle-switch-input')).toBeInTheDocument();
 		expect(
 			screen.getByText(
-				'When this is enabled, event 2 must occur after event 1, with any number of events in between. When this is disabled, events can be completed in any order. Nested criteria are not supported.'
+				'When this is enabled, event 2 must occur after event 1, with any number of events in between. When this is disabled, events can be completed in any order.'
 			)
 		).toBeInTheDocument();
 
@@ -126,7 +126,7 @@ describe('SegmentEditor', () => {
 
 		expect(
 			screen.getByText(
-				'When this is enabled, event 2 must occur after event 1, with any number of events in between. When this is disabled, events can be completed in any order. Nested criteria are not supported.'
+				'When this is enabled, event 2 must occur after event 1, with any number of events in between. When this is disabled, events can be completed in any order.'
 			)
 		).toBeInTheDocument();
 
