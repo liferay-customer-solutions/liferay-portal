7ee0bd71d6996fb8081088b9b9684990ad6b7fbf
Drew Brokke <drew.brokke@liferay.com>
2026-05-05 22:18:47 -0500

    LPD-87765 client-extension-test: relocate test to internal subpackage
    Source formatter requires the upgrade process test package to mirror
    the production class package.

--- modules/apps/client-extension/client-extension-test/src/testIntegration/java/com/liferay/client/extension/internal/upgrade/v3_5_2/test/CETConfigurationUpgradeProcessTest.java
+++ modules/apps/client-extension/client-extension-test/src/testIntegration/java/com/liferay/client/extension/internal/upgrade/v3_5_2/test/CETConfigurationUpgradeProcessTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.client.extension.upgrade.v3_5_2.test;
+package com.liferay.client.extension.internal.upgrade.v3_5_2.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.string.StringUtil;
