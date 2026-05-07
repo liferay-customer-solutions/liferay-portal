c095ab85b9b18f85ebd6b530e8829376c035a7f9
Alberto Sousa <alberto.sousa@liferay.com>
2026-05-07 10:27:28 -0300

    LPD-89002 Add integration test for the Commit executor registration

---
+++ modules/dxp/apps/content-site-generator/content-site-generator-test/src/testIntegration/java/com/liferay/content/site/generator/object/action/executor/test/CommitAIHubGenerationObjectActionExecutorTest.java
@@ -0,0 +1,279 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.content.site.generator.object.action.executor.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.list.type.model.ListTypeDefinition;
+import com.liferay.list.type.service.ListTypeDefinitionLocalService;
+import com.liferay.object.action.engine.ObjectActionEngine;
+import com.liferay.object.constants.ObjectActionTriggerConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.Serializable;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alberto Sousa
+ */
+@FeatureFlag("LPD-85514")
+@RunWith(Arquillian.class)
+public class CommitAIHubGenerationObjectActionExecutorTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_generationObjectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_AI_HUB_GENERATION", TestPropsValues.getCompanyId());
+		_generationItemObjectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_AI_HUB_GENERATION_ITEM", TestPropsValues.getCompanyId());
+
+		_group = GroupTestUtil.addGroup();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId(), TestPropsValues.getUserId());
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		for (String externalReferenceCode :
+				_listTypeDefinitionExternalReferenceCodes) {
+
+			ListTypeDefinition listTypeDefinition =
+				_listTypeDefinitionLocalService.
+					fetchListTypeDefinitionByExternalReferenceCode(
+						externalReferenceCode, TestPropsValues.getCompanyId());
+
+			if (listTypeDefinition != null) {
+				_listTypeDefinitionLocalService.deleteListTypeDefinition(
+					listTypeDefinition);
+			}
+		}
+
+		GroupTestUtil.deleteGroup(_group);
+	}
+
+	@Test
+	public void testExecute() throws Exception {
+		ObjectEntry generationObjectEntry =
+			_objectEntryLocalService.addObjectEntry(
+				0L, TestPropsValues.getUserId(),
+				_generationObjectDefinition.getObjectDefinitionId(), 0L, null,
+				HashMapBuilder.<String, Serializable>put(
+					"generationStatus", "ready"
+				).put(
+					"prompt", RandomTestUtil.randomString()
+				).put(
+					"title", RandomTestUtil.randomString()
+				).build(),
+				_serviceContext);
+
+		_addGenerationItemObjectEntry(
+			RandomTestUtil.randomString() + ".json",
+			generationObjectEntry.getObjectEntryId(), 1);
+		_addGenerationItemObjectEntry(
+			RandomTestUtil.randomString() + ".json",
+			generationObjectEntry.getObjectEntryId(), 2);
+
+		_objectActionEngine.executeObjectAction(
+			"commit", ObjectActionTriggerConstants.KEY_STANDALONE,
+			_generationObjectDefinition.getObjectDefinitionId(),
+			JSONUtil.put(
+				"classPK", generationObjectEntry.getObjectEntryId()
+			).put(
+				"objectEntry",
+				HashMapBuilder.putAll(
+					generationObjectEntry.getModelAttributes()
+				).put(
+					"values", generationObjectEntry.getValues()
+				).build()
+			),
+			TestPropsValues.getUserId());
+
+		Map<String, Serializable> values = _awaitCompletion(
+			generationObjectEntry.getObjectEntryId());
+
+		Assert.assertNotNull(values.get("commitDate"));
+		Assert.assertTrue(
+			Validator.isBlank(
+				GetterUtil.getString(values.get("failureReason"))));
+		Assert.assertEquals("committed", values.get("generationStatus"));
+
+		Assert.assertNotNull(
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					_listTypeDefinitionExternalReferenceCodes.get(0),
+					TestPropsValues.getCompanyId()));
+		Assert.assertNotNull(
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					_listTypeDefinitionExternalReferenceCodes.get(1),
+					TestPropsValues.getCompanyId()));
+	}
+
+	private void _addGenerationItemObjectEntry(
+			String fileName, long generationObjectEntryId, int loadOrder)
+		throws Exception {
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+
+		_listTypeDefinitionExternalReferenceCodes.add(externalReferenceCode);
+
+		FileEntry fileEntry = _dlAppLocalService.addFileEntry(
+			null, TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, fileName,
+			ContentTypes.APPLICATION_JSON,
+			JSONUtil.put(
+				"configuration",
+				JSONUtil.put(
+					"className",
+					"com.liferay.headless.admin.list.type.dto.v1_0." +
+						"ListTypeDefinition"
+				).put(
+					"multiCompany", true
+				).put(
+					"parameters",
+					JSONUtil.put(
+						"containsHeaders", "true"
+					).put(
+						"createStrategy", "UPSERT"
+					).put(
+						"importStrategy", "ON_ERROR_FAIL"
+					).put(
+						"updateStrategy", "UPDATE"
+					)
+				).put(
+					"taskItemDelegateName", "DEFAULT"
+				)
+			).put(
+				"items",
+				JSONUtil.putAll(
+					JSONUtil.put(
+						"externalReferenceCode", externalReferenceCode
+					).put(
+						"listTypeEntries", JSONFactoryUtil.createJSONArray()
+					).put(
+						"name", RandomTestUtil.randomString()
+					).put(
+						"system", true
+					))
+			).toString(
+			).getBytes(
+				StandardCharsets.UTF_8
+			),
+			null, null, null, _serviceContext);
+
+		_objectEntryLocalService.addObjectEntry(
+			0L, TestPropsValues.getUserId(),
+			_generationItemObjectDefinition.getObjectDefinitionId(), 0L, null,
+			HashMapBuilder.<String, Serializable>put(
+				"batchFile", fileEntry.getFileEntryId()
+			).put(
+				"fileName", fileName
+			).put(
+				"itemCount", 1
+			).put(
+				"loadOrder", loadOrder
+			).put(
+				"r_items_l_aiHubGenerationId", generationObjectEntryId
+			).build(),
+			_serviceContext);
+	}
+
+	private Map<String, Serializable> _awaitCompletion(
+			long generationObjectEntryId)
+		throws Exception {
+
+		long deadline = System.currentTimeMillis() + Time.MINUTE;
+
+		while (System.currentTimeMillis() < deadline) {
+			Thread.sleep(Time.SECOND / 2);
+
+			Map<String, Serializable> values =
+				_objectEntryLocalService.getValues(generationObjectEntryId);
+
+			String generationStatus = MapUtil.getString(
+				values, "generationStatus");
+
+			if (generationStatus.equals("committed") ||
+				generationStatus.equals("failed")) {
+
+				return values;
+			}
+		}
+
+		throw new AssertionError("Generation completion takes too long");
+	}
+
+	@Inject
+	private static DLAppLocalService _dlAppLocalService;
+
+	private static ObjectDefinition _generationItemObjectDefinition;
+	private static ObjectDefinition _generationObjectDefinition;
+	private static Group _group;
+	private static final List<String>
+		_listTypeDefinitionExternalReferenceCodes = new ArrayList<>();
+
+	@Inject
+	private static ListTypeDefinitionLocalService
+		_listTypeDefinitionLocalService;
+
+	@Inject
+	private static ObjectActionEngine _objectActionEngine;
+
+	@Inject
+	private static ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private static ObjectEntryLocalService _objectEntryLocalService;
+
+	private static ServiceContext _serviceContext;
+
+}
