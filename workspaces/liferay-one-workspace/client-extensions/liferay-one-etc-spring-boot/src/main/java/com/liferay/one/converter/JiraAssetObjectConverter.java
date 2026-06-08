a81e979d861bc4afde6aa314675b5bb9c9d30ba7
Amos Fong <amos.fong@liferay.com>
2026-06-08 15:40:03 -0700

    LPD-90459 SF

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/converter/JiraAssetObjectConverter.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/converter/JiraAssetObjectConverter.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.one.model;
+package com.liferay.one.converter;
 
 import com.liferay.portal.kernel.util.Validator;
 
@@ -13,21 +13,21 @@ import org.json.JSONObject;
 /**
  * @author Amos Fong
  */
-public class JiraAssetObject {
+public class JiraAssetObjectConverter {
 
 	protected String getAttributeKey(
-		JSONObject jsonObject, String attributeName) {
+		JSONObject jsonObject, String attributeId) {
 
 		return _getAttributeKey(
 			_getAttributeValueJSONObject(
-				jsonObject.getJSONArray("attributes"), attributeName));
+				jsonObject.getJSONArray("attributes"), attributeId));
 	}
 
 	protected String getAttributeValue(
-		JSONObject jsonObject, String attributeName) {
+		JSONObject jsonObject, String attributeId) {
 
 		JSONObject attributeValueJSONObject = _getAttributeValueJSONObject(
-			jsonObject.getJSONArray("attributes"), attributeName);
+			jsonObject.getJSONArray("attributes"), attributeId);
 
 		return attributeValueJSONObject.optString(
 			"displayValue", _getAttributeKey(attributeValueJSONObject));
@@ -49,16 +49,18 @@ public class JiraAssetObject {
 	}
 
 	private JSONObject _getAttributeValueJSONObject(
-		JSONArray attributesJSONArray, String attributeName) {
+		JSONArray attributesJSONArray, String attributeId) {
 
 		for (int i = 0; i < attributesJSONArray.length(); i++) {
 			JSONObject attributeJSONObject = attributesJSONArray.getJSONObject(
 				i);
 
-			String name = attributeJSONObject.optString(
-				"objectTypeAttributeName");
+			String objectTypeAttributeId = attributeJSONObject.optString(
+				"objectTypeAttributeId");
 
-			if (Validator.isNull(name) || !attributeName.equals(name)) {
+			if (Validator.isNull(objectTypeAttributeId) ||
+				!attributeId.equals(objectTypeAttributeId)) {
+
 				continue;
 			}
 
