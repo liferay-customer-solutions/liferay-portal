5dae7322c29a9cb05460c01dd2b3cb0ecfcd88c3
Beatriz Costa <beatriz.costa@liferay.com>
2026-07-22 11:23:35 -0300

    LPD-96496 Add the publisher details page

--- workspaces/liferay-one-workspace/scripts/bootstrap.sh
+++ workspaces/liferay-one-workspace/scripts/bootstrap.sh
@@ -72,6 +72,9 @@ function main {
 	echo "Setting virtual hosts."
 	bash scripts/bootstrap/set_virtual_hosts.sh
 
+	echo "Re-provisioning etc-spring-boot OAuth redirect URIs."
+	bash scripts/bootstrap/reprovision_etc_spring_boot_oauth.sh
+
 	echo "Seeding data."
 	bash scripts/seed.sh
 
